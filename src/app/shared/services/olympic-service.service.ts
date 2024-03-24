05cfe9d4e78e9529922eeabd4a0ef8fcca6bfd3f
Balde <abdourahamane620@gmail.com>
2024-03-24 13:18:29 +0100

    Configuration du service

--- src/app/shared/services/olympic-service.service.ts
+++ src/app/shared/services/olympic-service.service.ts
@@ -1,9 +1,20 @@
-import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { inject, Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { OlympicDto } from '../models/dto/olympic-dto';
 
 @Injectable({
   providedIn: 'root'
 })
 export class OlympicServiceService {
 
+  dataUrl: string = "http://localhost:3000/dataOlympic";
+  
+  private http = inject(HttpClient);
+
   constructor() { }
+
+  getAllOlympic(): Observable<OlympicDto[]>{
+    return this.http.get<OlympicDto[]>(this.dataUrl);
+  }
 }
