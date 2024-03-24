92481e854dacdf942b38ec7a2a09f8f44d899759
Balde <abdourahamane620@gmail.com>
2024-03-24 13:01:00 +0100

    Ajout du data json

--- src/app/views/home/home.component.ts
+++ src/app/views/home/home.component.ts
@@ -7,4 +7,27 @@ import { Component } from '@angular/core';
 })
 export class HomeComponent {
 
+  public view: [number,number] = [600, 370];
+  public showLabels: boolean = true;
+
+  public productSales = [
+    {
+      "name": "book",
+      "value": 5001
+    }, {
+      "name": "graphic card",
+      "value": 7322
+    }, {
+      "name": "desk",
+      "value": 1726
+    }, {
+      "name": "laptop",
+      "value": 2599
+    }, {
+      "name": "monitor",
+      "value": 705
+    }
+  ];
+  
+
 }
