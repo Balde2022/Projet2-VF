ffd112504c9d48dfa350216f6bf1f1e33be40d2b
Balde <abdourahamane620@gmail.com>
2024-03-24 13:23:06 +0100

    Injection et recuperation de données depuis le services

--- src/app/views/home/home.component.ts
+++ src/app/views/home/home.component.ts
@@ -1,14 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from 'express';
+import { OlympicDto } from '../../shared/models/dto/olympic-dto';
+import { Participation } from '../../shared/models/participation/participations';
+import { OlympicServiceService } from '../../shared/services/olympic-service.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+
+  olympic!: OlympicDto[];
+  participations!: Participation[];
+
+  public numberOfJOs:number = 0;
+  public numberOfCountry:number = 0;
+
 
   public view: [number,number] = [600, 370];
   public showLabels: boolean = true;
+
+  constructor( private router: Router, private olympicService: OlympicServiceService){
+  }
+  ngOnInit(): void {
+    throw new Error('Method not implemented.');
+  }
 
   public productSales = [
     {
@@ -29,5 +46,31 @@ export class HomeComponent {
     }
   ];
   
+
+  getAllOlympics(){
+    this.olympicService.getAllOlympic()
+    .subscribe({
+      next: (response: OlympicDto[]) => {
+        this.olympic = response;
+        this.getTotalMedals();
+      },
+      error:(error) => {
+        console.log(error);
+      },
+      complete:() => {}
+    });
+  }
+
+  getTotalMedals(){
+    for(let i of this.olympic){
+      this.numberOfCountry+=1;
+     this.participations = i.participations;
+     for(let j of this.participations){
+      i.totalMedals += j.medalsCount;
+      this.numberOfJOs+=1;
+     }
+    }
+    this.numberOfJOs/=this.numberOfCountry;
+  }
 
 }
