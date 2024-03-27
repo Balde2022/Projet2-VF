43ae56d46f6bba6dac41aa862c73adecb00f9287
Balde <abdourahamane620@gmail.com>
2024-03-27 21:02:06 +0100

    Clean code

--- src/app/views/home/home.component.ts
+++ src/app/views/home/home.component.ts
@@ -31,20 +31,8 @@ export class HomeComponent implements OnInit {
 
   public getOlimpic = [
     {
-      "name": "book",
-      "value": 5001,
-    }, {
-      "name": "graphic",
-      "value": 7322
-    }, {
-      "name": "desk",
-      "value": 1726
-    }, {
-      "name": "laptop",
-      "value": 2599
-    }, {
-      "name": "monitor",
-      "value": 705
+      "name": "",
+      "value": 0,
     }
   ];
   
